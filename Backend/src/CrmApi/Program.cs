3eacd6b933bab87b74d38e4de00035f45f349728
khaled <khaledouerani00@gmail.com>
2026-09-23 15:57:27 +0100

    fix: update Program.cs

--- Backend/src/CrmApi/Program.cs
+++ Backend/src/CrmApi/Program.cs
@@ -87,7 +87,7 @@ else if (connectionString.StartsWith("postgres://", StringComparison.OrdinalIgno
 var port = uri.Port > 0 ? uri.Port : 5432;
     connectionString =
         $"Host={uri.Host};" +
-        $"Port={uri.Port};" +
+        $"Port={Port};" +
         $"Database={database};" +
         $"Username={username};" +
         $"Password={password};";
